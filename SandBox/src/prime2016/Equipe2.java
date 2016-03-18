536037f2881ee591a308297c227facdd55759b5e
Sabrina <Sabrina@172.18.4.99>
2016-03-18 16:19:15 +0100

    Sabrina AÏT MOKHTAR

---
+++ SandBox/src/prime2016/Equipe2.java
@@ -0,0 +1,16 @@
+package prime2016;
+
+public class Equipe2 {
+	
+	public static void nomPrenom(){
+		System.out.print("Sabrina AÏT MOKHTAR");
+	}
+	
+	
+	public static void main(String[] args) {
+		Equipe2.nomPrenom() ;
+	}
+	
+	
+
+}
