0c738c375e0d1a25fc90b911b85e342ea3c9a3f5
Valentin <Valentin@172.18.21.8>
2016-03-18 16:35:53 +0100

    Valentin

--- SandBox/src/prime2016/equipe2.java
+++ SandBox/src/prime2016/equipe2.java
@@ -1,12 +1,12 @@
 package prime2016;
 
 public class equipe2 {
-	public static void AppelAlexandre () {
+	public static void AppelEquipe2 () {
 		System.out.println("Alexandre MARTY");
+		System.out.println("Valentin MAUDUIT");
 	}
 	
 	public static void main(String[] args) {
-		equipe2.AppelAlexandre();
+		equipe2.AppelEquipe2();
 	}
-	
 }
