291ea17657c7f06861be06a6149d3d5f83ac26d8
Alexandre MARTY <alexandre.marty@ymail.com>
2016-04-09 16:51:16 +0200

    test3

--- SandBox/src/prime2016/equipe2.java
+++ SandBox/src/prime2016/equipe2.java
@@ -1,12 +1,16 @@
 package prime2016;
 
 public class equipe2 {
-	public static void AppelAlexandre () {
+	public static void AppelEquipe2 () {
 		System.out.println("Alexandre MARTY");
+		System.out.println("Valentin MAUDUIT");
+		System.out.println("Sabrina AÏT MOKHTAR");
+		System.out.println("Sarah NABI");
 	}
 	
 	public static void main(String[] args) {
-		equipe2.AppelAlexandre();
+		equipe2.AppelEquipe2();
 	}
+	
 	
 }
